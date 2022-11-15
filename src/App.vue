af7bd63a41d871f7a6199a33509e9940ea9ccf4c
Nacir1i <113454578+Nacir1i@users.noreply.github.com>
2022-11-15 14:37:10 -0800

    deleted all item component to restart over

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div class="w-screen h-screen bg-primary flex justify-center items-center">
-    <div class="flex gap-6 justify-center items-center">
+    <div
+      class="w-full h-full md:h-[45rem] flex gap-6 justify-center items-center"
+    >
       <LogoSection />
-      <span
-        class="bg-secondary h-[40rem] w-2 md:block hidden rounded-full"
-      ></span>
-      <DisplaySection :list="items" />
+      <span class="w-3 h-full bg-third rounded-full"></span>
+      <div class="w-96 bg-white h-full"></div>
     </div>
   </div>
 </template>
 
 <script>
 import LogoSection from "./components/LogoSection.vue";
-import DisplaySection from "./components/DisplaySection.vue";
 
 export default {
   name: "App",
   components: {
     LogoSection,
-    DisplaySection,
   },
   data() {
     return {
-      items: [
+      list: [
         { id: 1, title: "item1", timer: "11:69", decription: "lolrandomxd" },
         { id: 2, title: "item2", timer: "11:69", decription: "lolrandomxd" },
         { id: 3, title: "item3", timer: "11:69", decription: "lolrandomxd" },
