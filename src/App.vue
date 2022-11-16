9c2404f13cac59e7d84f53960b45b95c7d690f6c
Nacir1i <113454578+Nacir1i@users.noreply.github.com>
2022-11-15 17:58:27 -0800

    create task component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,57 @@
 <template>
   <div class="w-screen h-screen bg-primary flex justify-center items-center">
     <div
-      class="w-full h-full md:h-[45rem] flex gap-6 justify-center items-center"
+      class="w-full h-full md:h-[45rem] flex gap-24 justify-center items-center"
     >
       <LogoSection />
       <span class="w-3 h-full bg-third rounded-full"></span>
-      <div class="w-96 bg-white h-full"></div>
+      <div class="w-[30rem] h-full">
+        <div class="w-full text-white flex justify-center items-center">
+          <div class="w-full h-24 flex justify-around items-center">
+            <h1 class="text-3xl font-bold">Create a new Task :</h1>
+            <div class="flex items-center justify-between">
+              <label
+                for="toggle"
+                class="inline-flex relative items-center cursor-pointer"
+              >
+                <input
+                  type="checkbox"
+                  v-model="display"
+                  id="toggle"
+                  class="sr-only peer"
+                />
+                <div
+                  class="w-9 h-5 bg-third peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-fith"
+                ></div>
+                <span
+                  class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >toggle</span
+                >
+              </label>
+            </div>
+          </div>
+        </div>
+        <CreateTask v-if="display" />
+        <div
+          :class="[
+            display ? 'h-96' : 'h-[40rem]',
+            'flex flex-col overflow-auto',
+          ]"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import LogoSection from "./components/LogoSection.vue";
+import CreateTask from "./components/CreateTask.vue";
 
 export default {
   name: "App",
   components: {
     LogoSection,
+    CreateTask,
   },
   data() {
     return {
@@ -25,6 +60,7 @@ export default {
         { id: 2, title: "item2", timer: "11:69", decription: "lolrandomxd" },
         { id: 3, title: "item3", timer: "11:69", decription: "lolrandomxd" },
       ],
+      display: false,
     };
   },
 };
---
+++ src/components/CreateTask.vue
@@ -0,0 +1,52 @@
+<template>
+  <form
+    @submit.prevent
+    class="w-full h-60 flex flex-col gap-y-4 justify-center items-center text-white border-b-4 border-third"
+  >
+    <div class="w-full flex justify-between items-center">
+      <label class="font-semibold text-md" for="name">What task is it ?</label>
+      <input
+        class="w-64 rounded-sm focus:outline-none bg-fith text-black p-1"
+        type="text"
+        v-model="name"
+        id="name"
+      />
+    </div>
+    <div class="w-full flex justify-between items-center">
+      <label class="font-semibold text-md" for="description"
+        >What is this task about ?</label
+      >
+      <input
+        class="w-64 rounded-sm focus:outline-none bg-fith text-black p-1"
+        type="text"
+        v-model="description"
+        id="description"
+      />
+    </div>
+    <div class="w-full flex justify-between items-center">
+      <label class="font-semibold text-md" for="name"
+        >when should it be done ?</label
+      >
+      <input
+        class="w-64 rounded-sm focus:outline-none bg-fith text-black p-1"
+        type="date"
+        v-model="date"
+        id="date"
+      />
+    </div>
+    <button class="w-1/3 p-1 bg-third rounded-md" type="submit">save</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "CreateTask",
+  data() {
+    return {
+      name: "",
+      description: "",
+      date: "",
+    };
+  },
+};
+</script>
